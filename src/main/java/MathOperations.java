5c8eee9846c91b92eba0f782d21849e30fd1931a
rbzf <anna.sadkowska@gmail.com>
2023-02-08 16:21:47 +0100

    Rozwiazanie

---
+++ src/main/java/MathOperations.java
@@ -0,0 +1,17 @@
+public class MathOperations {
+    boolean isEven(int x) {
+        return (x % 2) == 0;
+    }
+
+    boolean isOdd(int x) {
+        return (x % 2) != 0;
+    }
+
+    double circleField(double r) {
+        return 3.14 * r * r;
+    }
+
+    int power(int x) {
+        return x * x;
+    }
+}
